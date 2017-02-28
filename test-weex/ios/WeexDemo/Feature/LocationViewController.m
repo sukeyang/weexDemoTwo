34f07548e79ace804c6ec8219cd3e2851e232c69
yangshuo <410900345@qq.com>
2017-02-28 23:12:44 +0800

    清河修改：

--- test-weex/ios/WeexDemo/Feature/LocationViewController.m
+++ test-weex/ios/WeexDemo/Feature/LocationViewController.m
@@ -44,11 +44,11 @@
     });
      _instance = [[WXSDKInstance alloc] init];
     
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        [_instance fireGlobalEvent:@"geolocation" params:@{@"key":@"value"}];
-
-    });
-   }
+//    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+//        [_instance fireGlobalEvent:@"geolocation" params:@{@"key":@"value"}];
+//
+//    });
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
